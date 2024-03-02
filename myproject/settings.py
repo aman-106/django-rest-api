06b9befe875e44ad2447ad76b1339daf31c42b85
amandeep kumar <amanrags@gmail.com>
2024-03-02 15:12:49 +0530

    added cors

--- myproject/settings.py
+++ myproject/settings.py
@@ -37,10 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'patients'
+    'patients',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -131,4 +133,22 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
     )
 }
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000/',
+    'http://127.0.0.1:3000/'
+]
+
+CORS_ALLOW_METHODS = (
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+)
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+]
 
